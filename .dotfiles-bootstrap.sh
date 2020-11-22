0f3214f25471568fa088393a7aff8e8ef8889c04
Jackson Delahunt <jackson@stemn.com>
2020-11-23 00:49:31 +1100

    split-out

--- .dotfiles-bootstrap.sh
+++ .dotfiles-bootstrap.sh
@@ -11,7 +11,6 @@ wait
 
 # Clone large dotfiles and those that require post install
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-tmux.git &
-vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-x11.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-zsh.git &
 
 # Wait for dotfiles cloning to finish
@@ -48,6 +47,7 @@ vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-pulseaudio.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ranger.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-scripts.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ssh.git &
+vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-x11.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-xdg.git &
 
 # Wait for dotfiles cloning to finish
@@ -56,20 +56,3 @@ wait
 # Ignore wal-generated gtk files
 vcsh dotfiles-gtk ls-files $HOME | \
   xargs -n 1 vcsh dotfiles-gtk update-index --assume-unchanged
-
-# Start an x server for applications that require one to function (wal, code)
-export DISPLAY=:2
-vncserver $DISPLAY \
-  -autokill \
-  -SecurityTypes none \
-  -xstartup /usr/bin/i3
-
-# Install code settings sync extension, clear extension download history, start code so extensions install, forcibly terminate code after extensions install (race condition), generate wal cache
-code --install-extension Shan.code-settings-sync && \
-  sed -i '/lastDownload/d' $HOME/.config/Code/User/syncLocalSettings.json && \
-  code && sleep 90 && pkill -f code && \
-  code && sleep 5 && pkill -f code && \
-  ~/.config/scripts/wal.sh
-
-# Terminate x server and wait for shutdown
-kill -HUP $(pgrep Xtigervnc) && sleep 2
