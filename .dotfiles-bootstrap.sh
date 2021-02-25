12930128b5fdbcd031a341d82b2b478f133f9294
Jackson Delahunt <jackson@stemn.com>
2021-02-25 16:19:58 +1100

    30

--- .dotfiles-bootstrap.sh
+++ .dotfiles-bootstrap.sh
@@ -73,7 +73,7 @@ $HOME/.config/scripts/wal.sh
 # Install vscode extensions
 code --install-extension Shan.code-settings-sync && \
    sed -i '/lastDownload/d' $HOME/.config/Code/User/syncLocalSettings.json && \
-   code && sleep 90 && pkill -f code && \
+   code && sleep 30 && pkill -f code && \
    code && sleep 5 && pkill -f code
 
 # Terminate x server
