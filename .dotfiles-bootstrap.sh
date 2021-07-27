fa2ada160d0c5096930e774d0a6284752a6fb265
Jackson Delahunt <jackson@stemn.com>
2021-07-27 11:05:06 +1000

    search

--- .dotfiles-bootstrap.sh
+++ .dotfiles-bootstrap.sh
@@ -76,7 +76,7 @@ $HOME/.config/scripts/wal.sh
 # Install vscode extensions
 code --install-extension Shan.code-settings-sync && \
    sed -i '/lastDownload/d' $HOME/.config/Code/User/syncLocalSettings.json && \
-   code && sleep 5 && xdotool windowactivate $(xdotool search --name code) key "shift+alt+d" && \
+   code && xdotool search --sync --onlyvisible --class code windowfocus --sync %1 mousemove --window %1 0 0 key shift+alt+d && \
    sleep 30 && pkill -f code && code && sleep 5 && pkill -f code
 
 # Terminate x server
