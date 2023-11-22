14de4ef85843eb41417a39b5f47c102c3e519bed
Jackson Delahunt <jackson@stemn.com>
2023-11-22 14:21:45 +1100

    clean-up

--- .dotfiles-bootstrap.sh
+++ .dotfiles-bootstrap.sh
@@ -1,35 +1,4 @@
-# Install tmux plugin manager
-mkdir -p $HOME/.tmux/plugins/tpm >/dev/null 2>&1
-git clone --depth 1 https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm &
-
-# Install zsh plugin manager
-git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom" &
-
-# Wait for plugin managers to install
-wait
-
-# Fix bug where $HOME/.zshenv exists before zsh dotfiles have been cloned
-if [ ! -f $HOME/.zshrc ]; then
-  rm $HOME/.zshenv
-fi
-
-# Clone large dotfiles and those that require post install
-vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-tmux.git &
-vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-zsh.git &
-
-# Wait for dotfiles cloning to finish
-wait
-
-# Cache zsh plugins
-zsh -c "source $HOME/.zshrc" &
-
-# Cache tmux plugins
-zsh -c $HOME/.tmux/plugins/tpm/bin/install_plugins &
-
-# Wait for zsh and tmux caching to finish
-wait
-
-# Clone static dotfiles
+# Clone dotfiles
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-alacritty.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-autorandr.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-btop.git &
@@ -55,12 +24,33 @@ vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-pulseaudio.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ranger.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-scripts.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ssh.git &
+vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-tmux.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-x11.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-xava.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-xdg.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-zathura.git &
+vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-zsh.git &
 
 # Wait for dotfiles cloning to finish
+wait
+
+# Install tmux plugin manager
+mkdir -p $HOME/.tmux/plugins/tpm >/dev/null 2>&1
+git clone --depth 1 https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm &
+
+# Install zsh plugin manager
+git clone https://github.com/jandamm/zgenom.git $HOME/.zgenom &
+
+# Wait for plugin managers to install
+wait
+
+# Cache tmux plugins
+zsh -c $HOME/.tmux/plugins/tpm/bin/install_plugins &
+
+# Cache zsh plugins
+zsh -c "source $HOME/.zshrc" &
+
+# Wait for zsh and tmux caching to finish
 wait
 
 # Ignore pcmanfm changes
