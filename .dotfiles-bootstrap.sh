d67b12cc0cd4ff85b096f4b809a41fce42ff1647
Jackson Delahunt <jackson@stemn.com>
2026-01-31 14:35:18 +1100

    no-mime

--- .dotfiles-bootstrap.sh
+++ .dotfiles-bootstrap.sh
@@ -31,7 +31,6 @@ vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-i3.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-jobber.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ksnip.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ly.git &
-vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-mime.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-mpd.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-mpv.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-musikcube.git &
