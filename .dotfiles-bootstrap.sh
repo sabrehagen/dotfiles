23dc865ace7bcdc3cfae783e047277da59bf384d
Jackson Delahunt <jackson@stemn.com>
2026-01-31 20:41:25 +1100

    use-wal-directly

--- .dotfiles-bootstrap.sh
+++ .dotfiles-bootstrap.sh
@@ -75,7 +75,7 @@ vncserver $DISPLAY \
 # Generate wal cache for all wallpapers
 find $HOME/.local/share/wallpapers |
   grep -v bak | \
-  xargs -n 1 -P0 zsh $HOME/.config/scripts/wal.sh 2>/dev/null
+  xargs -n 1 -P0 $HOME/.local/bin/wal -e -n -s -t --saturate 0.3 -i 2>/dev/null
 
 # Install vs code wal extension
 yarn --cwd /opt/vscode-wal install-extension
